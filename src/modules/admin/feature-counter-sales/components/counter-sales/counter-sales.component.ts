3db62e81c8fb40b1252f7b0c59a2803b086ced64
DaThuTapCode <ntpdth2004@gmail.com>
2024-10-28 21:32:54 +0700

    Trong Phu cau hinh ban hang tai quay

--- src/modules/admin/feature-counter-sales/components/counter-sales/counter-sales.component.ts
+++ src/modules/admin/feature-counter-sales/components/counter-sales/counter-sales.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CounterSalesService } from '../../service/counter-sales.service';
+import { NotificationService } from '../../../../../shared/notification.service';
 
 @Component({
   selector: 'app-counter-sales',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter-sales.component.html',
   styleUrl: './counter-sales.component.scss'
 })
-export class CounterSalesComponent {
+export class CounterSalesComponent implements OnInit {
 
+  /**Biến hứng dữ liệu cho danh sách hóa đơn chờ thanh toán */
+  listPendingInvoice: any[] = [];
+
+  /**Biến hứng dữ liệu cho danh sách sản phẩm chi tiết  */
+  listProductDetail: any[] = [];
+
+  /**Biến hứng dữ liệu cho danh sách hóa đơn chi tiết lấy theo id hóa đơn */
+  listDetailInvoiceByIdInvoice: any[] = [];
+
+  /**Constructor */
+  constructor(
+    private counterSalesService: CounterSalesService,
+    private notiService: NotificationService
+  ) { }
+
+  /**Tải dữ liệu cho danh sách hóa đơn chờ thanh toán */  
+  fetchListPendingInvoice() {
+    this.counterSalesService.callApiGetListPendingInvoiceCounterSales().subscribe({
+      next: (response: any) => {
+        this.listPendingInvoice = response.data;
+      },
+      error: (err: any) => {
+
+      }
+    })
+  }
+
+  /**Hàm bắt sự kiện tạo hóa đơn chờ mới */
+  handleCreatePendingInvoice() {
+    throw new Error('Method not implemented.');
+  }
+
+  /**Khởi tạo dữ liệu */
+  ngOnInit(): void {
+    this.fetchListPendingInvoice();
+  }
 }
--- src/modules/admin/feature-counter-sales/service/counter-sales.service.ts
+++ src/modules/admin/feature-counter-sales/service/counter-sales.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+/**Service phục vụ nghiệp vụ bán hàng tại quầy */
 export class CounterSalesService {
+  /**Đường dẫn api cơ sở */
+  private apiBaseUrl: string = environment.apiUrl;
 
-  constructor() { }
+  //  GET
+  /**Đường dẫn api lấy danh sách hóa đơn chờ*/
+  private apiUrlGetListPendingInvoice: string = `${this.apiBaseUrl}/api/bhtq/list-pending-invoice`;
+
+  /**Đường dẫn lấy danh sách hóa đơn chi tiết theo id hóa đơn chờ */
+  private apiUrlGetListDetailInvoice: string = `${this.apiBaseUrl}/`;
+
+
+  //  POST
+  /**Đường dẫn api tạo hóa đơn chờ thanh toán*/
+  private apiPostCreateNewPendingInvoice: string = `${this.apiBaseUrl}/`;
+  /**Đường dẫn lấy danh sách sản phẩm chi tiết */
+  private apiUrlGetListProductDetail: string = `${this.apiBaseUrl}/`;
+
+  //  PUT
+  //  DELETE
+
+  /**Inject Dependencies*/
+  constructor(private http: HttpClient) { };
+
+
+  /**Gọi api lấy danh sách hóa đơn chờ thanh toán bán tại quầy */ 
+  callApiGetListPendingInvoiceCounterSales(): Observable<any> {
+    return this.http.get<any>(this.apiUrlGetListPendingInvoice);
+  }
+  /**Gọi api tạo hóa đơn chờ thanh toán tại quầy*/
+  callApiCreateNewPendingInvoice(): Observable<any> {
+    return new Observable;
+  };
+
+
 }
