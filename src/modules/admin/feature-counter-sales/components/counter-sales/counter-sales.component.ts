2c63fa0117e7695e68eb7a30ec2a6854ecd59687
TuanInfinity <tuanbuianh205@gmail.com>
2024-11-04 21:05:06 +0700

    TuanBA chuyen trang thai PGG, HD

--- src/modules/admin/feature-counter-sales/components/counter-sales/counter-sales.component.ts
+++ src/modules/admin/feature-counter-sales/components/counter-sales/counter-sales.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { CounterSalesService } from '../../service/counter-sales.service';
 import { NotificationService } from '../../../../../shared/notification.service';
 import {
@@ -30,14 +30,25 @@ import { KichCoService } from '../../../feature-attribute-management/service/kic
 import { ChatLieuService } from '../../../feature-attribute-management/service/chat-lieu.service';
 import { KhachHangResponse } from '../../../../../models/khach-hang/response/khach-hang-response';
 import { Route, Router, ActivatedRoute } from '@angular/router';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+
+export enum StatusHD {
+  PENDING = 'PENDING',
+  PAID = 'PAID',
+  CANCELLED = 'CANCELLED',
+  REFUNDED = 'REFUNDED',
+  OVERDUE = 'OVERDUE'
+}
 
 @Component({
   selector: 'app-counter-sales',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './counter-sales.component.html',
   styleUrl: './counter-sales.component.scss'
 })
+
 export class CounterSalesComponent implements OnInit {
   form!: FormGroup; //Biến form
   searchResults: any[] = []; // Biến lưu trữ kết quả tìm kiếm
@@ -53,6 +64,7 @@ export class CounterSalesComponent implements OnInit {
 
   //Biến hứng dữ liệu khách hàng
   khachHangs: KhachHangResponse[] = [];
+
 
   selectedCustomer: KhachHangResponse = {
     idKhachHang: 0,
@@ -119,6 +131,9 @@ export class CounterSalesComponent implements OnInit {
     idKichCo: null,
   };
 
+  /** Biến điều khiển modal */
+  isConfirmModalVisible = false;
+  isCustomerSelectModalVisible = false;
 
 
   /** Constructor */
@@ -136,7 +151,25 @@ export class CounterSalesComponent implements OnInit {
     private kichCoService: KichCoService,
     private chatLieuService: ChatLieuService,
     private fb: FormBuilder
-  ) {}
+  ) { }
+
+  /** Hàm bắt dữ liệu trạng thái của hóa đơn */
+  getInvoiceStatus(status: string): string {
+    switch (status) {
+      case StatusHD.PENDING:
+        return 'Đang chờ';
+      case StatusHD.PAID:
+        return 'Đã thanh toán';
+      case StatusHD.CANCELLED:
+        return 'Đã hủy';
+      case StatusHD.REFUNDED:
+        return 'Đã hoàn tiền';
+      case StatusHD.OVERDUE:
+        return 'Quá hạn';
+      default:
+        return 'Không xác định';
+    }
+  }
 
   /**Tải dữ liệu cho danh sách hóa đơn chờ thanh toán */
   fetchListPendingInvoice() {
@@ -287,7 +320,7 @@ export class CounterSalesComponent implements OnInit {
   handleCreatePendingInvoice() {
     this.counterSalesService.callApiCreateNewPendingInvoice().subscribe({
       next: (response: any) => {
-        if(response.status === 200){
+        if (response.status === 200) {
           this.fetchListPendingInvoice();
           this.notiService.showSuccess(response.message);
           this.activeTab = this.listPendingInvoice.length;
@@ -392,7 +425,58 @@ export class CounterSalesComponent implements OnInit {
     this.closeModal('closeModalSelectedCustomer');
   }
 
+  /** Hàm xác nhận thanh tóan */
+  openConfirmModalPay() {
+    this.isConfirmModalVisible = true;
+  }
 
+  /** Hàm hủy xác nhận thanh toán */
+  cancelPay() {
+    this.isConfirmModalVisible = false;
+  }
+
+  /** Hàm kiểm tra và thanh toán */
+  confirmPayment() {
+    const hd = this.listPendingInvoice[this.activeTab];
+    if (!hd.hoaDonChiTiet || hd.hoaDonChiTiet.length === 0) {
+      this.notiService.showError('Thêm sản phẩm vào hóa đơn');
+      return;
+    }
+
+    /** Lấy giá trị của radio button */
+    const paymentMethod = document.querySelector('input[name="flexRadioDefault"]:checked') as HTMLInputElement;
+
+    /** Kiểm tra xem người dùng đã chọn phương thức thanh toán chưa */
+    if (!paymentMethod) {
+      this.notiService.showError('Vui lòng chọn phương thức thanh toán');
+      return;
+    }
+
+    /**Gọi Api để xác nhận thanh toán */
+    this.counterSalesService.callApiPayInvoice(hd.idHoaDon).subscribe({
+      next: (response: any) => {
+        if (response.status === 200) {
+          // Cập nhật trạng thái hóa đơn sau khi thanh toán thành công
+          hd.trangThai = StatusHD.PAID; // Sử dụng enum để cập nhật trạng thái
+          this.notiService.showSuccess('Thanh toán thành công!');
+
+          // Cập nhật lại thông tin trạng thái hiển thị
+          hd.trangThaiHienThi = this.getInvoiceStatus(hd.trangThai); // Cập nhật trạng thái hiển thị
+          this.fetchListPendingInvoice(); // Tải lại danh sách hóa đơn chờ
+        } else {
+          this.notiService.showError('Có lỗi xảy ra khi thanh toán: ' + response.message);
+        }
+      },
+      error: (err: any) => {
+        this.notiService.showError('Thanh toán không thành công: ' + err.message);
+      }
+    });
+
+    console.log('Tiến hành thanh toán');
+    this.isConfirmModalVisible = false;
+  }
+
+  /**Hàm gọi check hóa đơn có chưa sản phảm không */
 
 
   /**Khởi tạo dữ liệu */
